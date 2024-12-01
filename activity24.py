b0cadcf0a45c94d41571e968e063a7888f155f6e
Jayvee Garcia <jayveegarcia018@gmail.com>
2024-12-02 07:47:41 +0800

    New

---
+++ activity24.py
@@ -0,0 +1,6 @@
+from activity23 import add
+from code_challenge1 import asterisk 
+
+add(21,21)
+asterisk()
+help(add) 
---
+++ activity25.py
@@ -0,0 +1,9 @@
+# def act25():
+fruits = ["Banana" , "Apple" , "Starapple" , "Pineapple"]
+    
+fruits.pop()
+fruits.remove("Banana")
+fruits.append("Grapes")
+fruits.insert(3, "Orange")
+    
+print(fruits)
