c93fa50d750f7ac4f1a07917f0558ea3d136fb87
Jayvee Garcia <jayveegarcia018@gmail.com>
2024-12-17 22:57:05 +0800

    Delete activity5.py

--- activity5.py
+++
@@ -1,8 +0,0 @@
-def act5():
-    
-    temp = int(input("Enter the temperature in farenheit: "))
-
-    celcius = (temp - 32) * 5/9
-    converted = (round(celcius,2))
-
-    print(f"The Conversion of {temp} degrees farenheit is {converted}")
