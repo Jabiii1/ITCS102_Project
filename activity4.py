a8397d28949d8f032563e6c184afca199a711e08
Jayvee Garcia <jayveegarcia018@gmail.com>
2024-12-17 22:55:35 +0800

    Delete activity4.py

--- activity4.py
+++
@@ -1,7 +0,0 @@
-def act4():
-    num1 = eval(input("Enter your first number  -->"))
-    num2 = eval(input("Enter your second number -->"))
-
-    sum = num1 + num2
-
-    print("The sum of " , num1 , "+" , num2 , "=" , sum)
