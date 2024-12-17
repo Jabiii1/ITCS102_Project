841bb7140a2ce55d060b3915c21556f00c13a9b7
Jayvee Garcia <jayveegarcia018@gmail.com>
2024-12-17 23:02:29 +0800

    Delete code_challenge5.py

--- code_challenge5.py
+++
@@ -1,9 +0,0 @@
-def cc5():
-
-    print("===FAHRENHEIT TO CELSIUS CONVERTER===")
-
-    fahrenheit = eval(input("\nEnter the temperature in Fahrenheit: "))
-    celsius = ((fahrenheit * 32) * 5 / 9)
-
-
-    print(f"{fahrenheit}°F converted to celsius is {round(celsius)}°C.")
